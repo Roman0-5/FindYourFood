e019819033ee72248dc51708bd77603a37b274e2
KevinCuculic <herzog-graf@hotmail.com>
2025-05-11 21:37:51 +0200

    Weiterleitung zu den jeweiligen tabs Startseite tauchtautomatisch auf!

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000;
 // Statische Dateien bereitstellen
 app.use(express.static(path.join(__dirname)));
 
+// 💡 Leitet automatisch auf die Startseite um!
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "htmls", "StartSite.html"));
+});
+
 // Server starten
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
